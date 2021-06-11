971d0691e037d0123c0970b3770ddaabc66f2b23
Rαjαt Ҝαsђyαp <50959267+STRK-ND@users.noreply.github.com>
2021-06-11 12:03:37 +0530

    AOSP

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common NusantaraROM stuff
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common AEX_ROM stuff
+$(call inherit-product, vendor/aosp/common.mk)
 
 #Gapps
 USE_GAPPS=true
@@ -43,7 +43,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00TD
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := nusantara_X00TD
+PRODUCT_NAME := aosp_X00TD
 PRODUCT_MODEL := Zenfone Max Pro (M1)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
